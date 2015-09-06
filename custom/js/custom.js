16d3b97fa8161e1cfb77fc6f51f0a6f21fd156ef
artemilchuk <artem.ilchuk@ed-era.com>
2015-09-06 23:32:43 +0300

    Update custom/js/custom.js

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -6,4 +6,14 @@ $(document).ready(function () {
     //href = "https://ed-era.com/books/" + _.last(href.split("-"));
     href = "http://" + _.last(href.split("-")) + ".ed-era.com";
     $(".custom-link:last").prop("href", href);
+
+    /* Question after topic */
+    $(".chapter").click((".quiz-question-explanation").hide());
+    $("button.show").click(function(){
+        $( this ).parent().find("p.quiz-question-explanation").show();
+    });
+    $(".hide").click(function(){
+        $( this ).parent().find("p.quiz-question-explanation").hide();
+        });
+
 });
