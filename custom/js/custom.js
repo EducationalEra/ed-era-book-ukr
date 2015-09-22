d2b20dff31d1c48b0fed9744fe3009330e621d9c
Olga Filipova <chudaol@gmail.com>
2015-09-22 23:34:59 +0200

    added all custom js functions to the page change event handler.

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,18 +1,24 @@
 $(document).ready(function () {
-  var href, $lastLink;
-  
-  $("a.toggle-search").remove();
-  
-  $lastLink = $(".custom-link:last");
-  href = $lastLink.prop("href");
-  href = "https://ed-era.com/books/" + "ukrainian";
-  $lastLink.prop("href", href);
-});
+  function init () {
+    var href, $lastLink;
 
-/* Question after topic */
-$(document).on('click', 'button.show', function(){
-        $( this ).parent().find("p.quiz-question-explanation").removeClass( "hide" );
-});
-$(document).on('click', 'button.hide', function(){
-        $( this ).parent().find("p.quiz-question-explanation").addClass( "hide" );
+    $("a.toggle-search").remove();
+
+    $lastLink = $(".custom-link:last");
+    href = $lastLink.prop("href");
+    href = "https://ed-era.com/books/" + "ukrainian";
+    $lastLink.prop("href", href);
+    /* Question after topic */
+    $(document).on('click', 'button.show', function(){
+      $( this ).parent().find("p.quiz-question-explanation").removeClass( "hide" );
+    });
+    $(document).on('click', 'button.hide', function(){
+      $( this ).parent().find("p.quiz-question-explanation").addClass( "hide" );
+    });
+  }
+
+  init();
+  require(["gitbook"], function (gitbook) {
+    gitbook.events.bind("page.change", init);
+  });
 });
--- custom/js/popup.js
+++ custom/js/popup.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-   $( "div.popup" ).replaceWith( "<div class='" + "description" + "'><div class='" + "text" +"'>Ще більше відеоматеріалів в <a class='"+"knopka1"+"' href='" + "https://study.ed-era.com/courses/EdEra/u102/U102/about" + "' target='" + "_blank" + "'>онлайн-курсі</a> від розробників цієї книги</div><div class='" + "btn-knopka" +"'><a class='"+"knopka"+"' href='" + "https://study.ed-era.com/courses/EdEra/u102/U102/about" + "'   target='" + "_blank" + "'>Перейти на курс</a></div>" );
+$(document).ready(function() {
+   function init () {
+      $( "div.popup" ).replaceWith( "<div class='" + "description" + "'><div class='" + "text" +"'>Ще більше відеоматеріалів в <a class='"+"knopka1"+"' href='" + "https://study.ed-era.com/courses/EdEra/u102/U102/about" + "' target='" + "_blank" + "'>онлайн-курсі</a> від розробників цієї книги</div><div class='" + "btn-knopka" +"'><a class='"+"knopka"+"' href='" + "https://study.ed-era.com/courses/EdEra/u102/U102/about" + "'   target='" + "_blank" + "'>Перейти на курс</a></div>" );
+   }
+
+   init();
+   require(["gitbook"], function (gitbook) {
+      gitbook.events.bind("page.change", init);
    });
+});
