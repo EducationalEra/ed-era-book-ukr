03a6a42f1f70e40e73f23f30bbc5466aeaa982fb
artemilchuk <artem.ilchuk@ed-era.com>
2017-12-09 14:51:54 +0200

    fix mathjax

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -21,6 +21,22 @@ $(document).ready(function () {
 
   init();
   require(["gitbook"], function (gitbook) {
+    function startMathJax() {
+      MathJax.Hub.Config({
+        tex2jax: {}
+      });
+
+      gitbook.events.bind("page.change", function () {
+        MathJax.Hub.Typeset();
+      });
+    }
     gitbook.events.bind("page.change", init);
+    var interval = null
+    interval = setInterval(function () {
+      if (typeof MathJax === "object") {
+        startMathJax()
+        clearInterval(interval);
+      }
+    }, 1000);
   });
 });
